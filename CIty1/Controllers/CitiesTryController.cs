3206c6001ac9dcb6a76a678c784fb077f7083622
Rdsarkar <rajapscl@gmail.com>
2021-09-22 17:50:08 +0600

    GetById, UpdateData, InsertData Custom Response

--- CIty1/Controllers/CitiesTryController.cs
+++ CIty1/Controllers/CitiesTryController.cs
@@ -10,6 +10,18 @@ using CIty1.DTOs;
 
 namespace CIty1
 {
+    public class SelfClass 
+    {
+        public decimal Id { get; set; }
+    }
+
+    public class SelfClass2
+    {
+        public decimal Id { get; set; }
+        public string Name { get; set; }
+        public string Countrycode { get; set; }
+        public decimal? Population { get; set; }
+    }
     [Route("api/[controller]")]
     [ApiController]
     public class CitiesTryController : ControllerBase
@@ -28,7 +40,10 @@ namespace CIty1
             //return await _context.Cities.OrderByDescending(s => s.Population)
             //                     .ToListAsync();
 
-            List<City> cities= await _context.Cities.OrderBy(s => s.Name)
+            //return await _context.Cities.OrderBy(s => s.Name)
+            //                     .ToListAsync();
+
+            List<City> cities= await _context.Cities.OrderBy(s => s.Id)
                                  .ToListAsync();
 
             if (cities.Count>0) 
@@ -48,79 +63,205 @@ namespace CIty1
                     Success = false,
                     Payload = null
                 });
-            }
-
-            //return await _context.Cities.OrderBy(s => s.Name)
-            //                     .ToListAsync();
-            
+            }         
         }
 
         // GET: api/Cities/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<City>> GetCity(decimal id)
+        [HttpPost("GetCityById")]
+        public async Task<ActionResult<ResponseDto>> GetCity([FromBody] SelfClass input)
         {
-            var city = await _context.Cities.FindAsync(id);
+            if (input.Id == 0)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, new ResponseDto 
+                {
+                    Message ="input error",
+                    Success =false,
+                    Payload = null
+                });
+            }
+           
+            var city = await _context.Cities.Where(c => c.Id == input.Id).FirstOrDefaultAsync();
 
             if (city == null)
             {
-                return NotFound();
+                return StatusCode(StatusCodes.Status404NotFound, new ResponseDto
+                {
+                    Message = "Data Base has no data",
+                    Success = false,
+                    Payload = null
+                });
             }
 
-            return city;
+            else 
+            {
+                return StatusCode(StatusCodes.Status200OK, new ResponseDto
+                {
+                    Message = "Data Found",
+                    Success = true,
+                    Payload = city
+                });
+            }
+            
+            
         }
 
         // PUT: api/Cities/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutCity(decimal id, City city)
+        [HttpPost("UpdateCity")]
+        public async Task<ActionResult<ResponseDto>> PutCity([FromBody] City input)
         {
-            if (id != city.Id)
+            if (input.Id == 0)
             {
-                return BadRequest();
-            }
-
-            _context.Entry(city).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!CityExists(id))
+                return StatusCode(StatusCodes.Status400BadRequest, new ResponseDto 
                 {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                    Message="You have to type the ID",
+                    Success=false,
+                    Payload=null
+                });
             }
 
-            return NoContent();
+            if (input.Name == null)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, new ResponseDto
+                {
+                    Message = "Name Cant be Null!",
+                    Success = false,
+                    Payload = null
+                });
+            }
+
+            if (input.Countrycode == null)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, new ResponseDto
+                {
+                    Message = "Countrycode Cant be Null!",
+                    Success = false,
+                    Payload = null
+                });
+            }
+            if (input.Population == 0)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, new ResponseDto
+                {
+                    Message = "You have to type the Population Number!",
+                    Success = false,
+                    Payload = null
+                });
+            }
+
+
+            City city = await _context.Cities.Where(i => i.Id == input.Id).FirstOrDefaultAsync();
+            if (city==null) 
+            {
+                return StatusCode(StatusCodes.Status404NotFound, new ResponseDto 
+                {
+                    Message= "DATA IS NOT FOUND!!",
+                    Success= false,
+                    Payload= null
+                });
+            }
+
+            city.Name = input.Name;
+            city.Countrycode = input.Countrycode;
+            city.Population = input.Population;
+            _context.Cities.Update(city);
+            bool isSaved = await _context.SaveChangesAsync() > 0;
+
+            if (isSaved == false)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseDto
+                {
+                    Message = "update cant be execute server error!!",
+                    Success = false,
+                    Payload = null
+                });
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status200OK, new ResponseDto 
+                {
+                    Message = "Update Complete",
+                    Success = true,
+                    Payload = null
+                });
+            }
         }
 
         // POST: api/Cities
-        [HttpPost]
-        public async Task<ActionResult<City>> PostCity(City city)
+        [HttpPost("InsertingNewCity")]
+        public async Task<ActionResult<ResponseDto>> PostCity([FromBody] City input)
         {
-            _context.Cities.Add(city);
-            try
+            if (input.Id == 0)
             {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateException)
-            {
-                if (CityExists(city.Id))
+                return StatusCode(StatusCodes.Status400BadRequest, new ResponseDto
                 {
-                    return Conflict();
-                }
-                else
-                {
-                    throw;
-                }
+                    Message = "You have to type the ID",
+                    Success = false,
+                    Payload = null
+                });
             }
 
-            return CreatedAtAction("GetCity", new { id = city.Id }, city);
+            if (input.Name == null)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, new ResponseDto
+                {
+                    Message = "Name Cant be Null!",
+                    Success = false,
+                    Payload = null
+                });
+            }
+
+            if (input.Countrycode == null)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, new ResponseDto
+                {
+                    Message = "Countrycode Cant be Null!",
+                    Success = false,
+                    Payload = null
+                });
+            }
+            if (input.Population == 0)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, new ResponseDto
+                {
+                    Message = "You have to type the Population Number!",
+                    Success = false,
+                    Payload = null
+                });
+            }
+            //till now is ok
+            //old
+            City city1 = await _context.Cities.Where(a => a.Id == input.Id).FirstOrDefaultAsync();
+            if (city1 != null) 
+            {
+                return StatusCode(StatusCodes.Status409Conflict, new ResponseDto 
+                {
+                    Message = "Data Already exists",
+                    Success = false,
+                    Payload = null
+                });
+            }
+
+            _context.Cities.Add(input);
+            bool isSaved = await _context.SaveChangesAsync() > 0 ;
+            if (isSaved == false) 
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseDto
+                {
+                    Message = "Data Saved Failed",
+                    Success = false,
+                    Payload = null
+                });
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status200OK, new ResponseDto
+                {
+                    Message = "Data Successfully Saved",
+                    Success = true,
+                    Payload = null
+                });
+            }
+
         }
 
         // DELETE: api/Cities/5
